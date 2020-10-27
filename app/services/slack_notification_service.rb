8a11e79eb3ac9785af62de62fa1e08605d5d3431
kento matsushima <nyanpucchi1028@gmail.com>
2020-10-27 21:05:14 +0900

    fix

--- app/services/slack_notification_service.rb
+++ app/services/slack_notification_service.rb
@@ -1,25 +1,27 @@
 class SlackNotificationService
   DEFAULT_TIMEOUT = 3
+
   class << self
-    def info(title,message,option={})
-      notify(:info,title,message,option)
+    def info(title, message, option = {})
+      notify(:info, title, message, option)
     end
 
-    def warn(title,message,option={})
-      notify(:warn,title,message,option)
+    def warn(title, message, option = {})
+      notify(:warn, title, message, option)
     end
 
     private
-    def notify(type,title,message,option)
+
+    def notify(type, title, message, option)
       return if Rails.env.test?
 
       params = {
           color: 'good',
           channel: 'system_notification',
       }
-      notifier = Slack::Notifier.new(ENV.fetch("SLACK_WEBHOOK_URL"){""}, http_options: { open_timeout: DEFAULT_TIMEOUT })
+      notifier = Slack::Notifier.new(ENV.fetch("SLACK_WEBHOOK_URL"), http_options: {open_timeout: DEFAULT_TIMEOUT})
       begin
-        notifier.post(attachments: {title: title, text: message, color: param[:color]}, channel: param[:channel], at: param[:at])
+        notifier.post(attachments: {title: title, text: message, color: param[:color]}, channel: param[:channel])
       rescue => e
         Rails.logger.error("Slack通知に失敗しました。title:#{title}, message:#{message}")
       end
